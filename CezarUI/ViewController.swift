de156ba3f06c840ad494653fdc673f9d0873ade7
ArtemVasilenko <artemvasilenko88@gmail.com>
2019-06-25 19:04:09 +0300

    Initial Commit

---
+++ CezarUI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CezarUI
+//
+//  Created by Артем on 6/25/19.
+//  Copyright © 2019 Артем. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
